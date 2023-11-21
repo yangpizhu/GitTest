1bb4ec698dbfd648ec2f9d8163def24d0aa4fc94
yangpizhu <yangpizhu@qq.com>
2023-11-21 23:55:18 +0800

    测试第一次在IDEA中提交

---
+++ src/main/java/com/yangpizhu/GitTest.java
@@ -0,0 +1,16 @@
+package com.yangpizhu;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: yangpizhu
+ * Date: 2023/11/21
+ * Time: 23:52
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
